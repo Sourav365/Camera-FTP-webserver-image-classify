aa2264d2a58d17066186ceeb5039ca9cd0bea1cf
Sourav Das <49667585+Sourav365@users.noreply.github.com>
2021-03-03 23:32:34 +0530

    Update uploadData.h

--- Smart_Glass_v3/uploadData.h
+++ Smart_Glass_v3/uploadData.h
@@ -1,4 +1,3 @@
-//https://esp-32-cam.000webhostapp.com/updatedata.php?Object1=Som&Value1=9.9&Object2=Sourav&Value2=89.9&Object3=Sommm&Value3=90.9&Object4=Soma&Value4=99.07&Object5=Sonu&Value5=99.9&Data=
 String urlencode(String);
  
 void uploadImg(camera_fb_t *fb){
@@ -12,52 +11,21 @@ void uploadImg(camera_fb_t *fb){
   String buffer = base64::encode((uint8_t *) fb->buf, fb->len);
 
   buffer = urlencode(buffer);
-  //String data = ("Object1=Som&Value1=9.9&Object2=Sourav&Value2=89.9&Object3=Sommm&Value3=90.9&Object4=Soma&Value4=99.07&Object5=Sonu&Value5=99.9&Data=" + buffer);
-  String data = ("Object1="+object[0]+"&Value1="+String(value[0])+"&Object2="+object[1]+"&Value2="+String(value[1])+"&Object3="+object[2]+"&Value3="+String(value[2])+"&Object4="+object[3]+"&Value4="+String(value[3])+"&Object5="+object[4]+"&Value5="+String(value[4])+"&Data=" + buffer);
-  //String data = ("Object1="+object[0]+"&Value1=1"+"Object2="+object[1]+"&Value2=2"+"Object3="+object[2]+"&Value3=3"+"Object4="+object[3]+"&Value4=4"+"Object5="+object[4]+"&Value5=5"+"&Data=" + buffer);
-  //String data = ("Object1="+String(object[0])+"&Value1="+String(value[0])+"Object2="+String(object[1])+"&Value2="+String(value[1])+"Object3="+String(object[2])+"&Value3="+String(value[2])+"Object4="+String(object[3])+"&Value4="+String(value[3])+"Object5="+String(object[4])+"&Value5="+String(value[4])+"&Data=" + buffer);
-
-  //String data = "{\"Object1\":\"Sourav\",\"Value1\":98.3,\"Object2\":\"Namrata\",\"Value2\":98.3,\"Object3\":\"Peter2\",\"Value3\":98.3,\"Object4\":\"Peter3\",\"Value4\":98.3,\"Object5\":\"Souraaavv\",\"Value5\":98.3,\"Data\":\"Hiiiiiii\"}";//":\"" + buffer + "\"}";
-  
+  String data = ("Object1="+object[0]+"&Value1="+String(value[0])+"&Object2="+object[1]+"&Value2="+String(value[1])+"&Object3="+object[2]+"&Value3="+String(value[2])+"&Object4="+object[3]+"&Value4="+String(value[3])+"&Object5="+object[4]+"&Value5="+String(value[4])+"&Data=" + buffer);  
   Serial.println(data);
   
-  /*WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
-    return;
-  }*/
-  
- 
-  /*client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
-               "Connection: close\r\n\r\n");*/
 
-   /*client.println("POST / HTTP/1.1");
-   client.print("Host: ");
-   client.println(host);
-   client.println("Accept: '*'/*");
-   client.println("Content-Type: application/x-www-form-urlencoded");
-   client.print("Content-Length: ");
-   client.println(url.length());
-   client.println();
-   client.print(url);*/
+  HTTPClient http;
+  http.begin("https://xyz.000webhostapp.com/postdata.php");
+  http.addHeader("Content-Type","application/x-www-form-urlencoded");
+  int httpResponseCode = http.POST(data);
+  String payload = http.getString();
 
-    HTTPClient http;
-    http.begin("https://esp-32-cam.000webhostapp.com/postdata.php");
-    http.addHeader("Content-Type","application/x-www-form-urlencoded");
-    int httpResponseCode = http.POST(data);
-    String payload = http.getString();
-
-   delay(2000);//500
-   Serial.println(payload);
-   Serial.print("HTTP Response code: ");
-   Serial.println(httpResponseCode);
+  delay(2000);//500
+  Serial.println(payload);
+  Serial.print("HTTP Response code: ");
+  Serial.println(httpResponseCode);
    
-  /*while(client.available()){
-    String line = client.readStringUntil('\r\n\r\n');
-    Serial.println(line);
-  }*/
 
   Serial.println("Close connection");
   delay(3000);
